8e1da962722f6e9e6481b0c88e888ca43934bea8
Omru Jawara <omaru.jawara94@gmail.com>
2023-01-18 19:03:27 -0500

    StackPanel

---
+++ StackPanel.ps1
@@ -0,0 +1,22 @@
+﻿Add-Type -AssemblyName PresentationFramework
+
+
+[xml]$xaml = @'
+<Window
+    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+    x:Name="Window" 
+    Title="StackPanel" Width="750" Height="500" WindowStartupLocation="CenterScreen">
+    
+    <StackPanel Width="75" Height="75">
+        <Button Content="Button(1)" />
+        <Button Content="Button(2)" />
+        <Button Content="Button(3)" />
+    </StackPanel>
+
+
+</Window>
+'@
+
+$window = [System.Windows.Markup.XamlReader]::Load( [System.Xml.XmlNodeReader]$xaml)
+$window.ShowDialog()
